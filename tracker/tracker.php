6da8112ac269b07498c0fa6e3c071b899681aba9
jm11116 <74529681+jm11116@users.noreply.github.com>
2021-10-26 17:56:59 +1100

    Update to Mailer

--- tracker/tracker.php
+++ tracker/tracker.php
@@ -5,9 +5,10 @@ require_once "classes/settings_getter.php";
 require_once "classes/block_checker.php";
 require_once "classes/tracking.php";
 require_once "classes/writer.php";
+require_once "classes/mailer.php";
 
 $check = new BlockChecker();
-if ($check->blocked == "true"){
+if ($check->blocked === "true"){
     exit();
 } else {
     new Mailer(); //Calls parent, which is Writer, which calls their parents
